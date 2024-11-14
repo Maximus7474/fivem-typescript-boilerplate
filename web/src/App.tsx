1876caed4349d4d5a3072d0fd147dda123eb684a
Maximus7474 <the.maximus.7474@gmail.com>
2024-11-14 13:53:53 +0100

    rf(web/App): use Mantine template

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 import { isEnvBrowser } from './utils/misc';
 import { useNuiEvent } from './hooks/useNuiEvent';
 import { fetchNui } from './utils/fetchNui';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Title, Text, Flex, Button } from '@mantine/core';
 
 function App() {
+  const [opened, { open, close }] = useDisclosure(isEnvBrowser());
   const [visible, setVisible] = useState(isEnvBrowser());
   const [count, setCount] = useState(0);
 
   useNuiEvent('setVisible', (data: { visible?: boolean }) => {
     setVisible(data.visible || false);
+    if (data.visible) open();
   });
 
   function handleHideModal() {
+    close();
     setVisible(false);
     void fetchNui('exit');
   }
@@ -19,17 +24,16 @@ function App() {
   return (
     <>
       {visible && (
-        <div className="boilerplate-wrapper">
-          <div className="boilerplate-modal-container">
-            <h3>Boilerplate Modal</h3>
-            <p>Count: {count}</p>
+        <div className="nui-wrapper">
+          <Modal opened={opened} onClose={handleHideModal} centered={true} >
+            <Title order={3}>Boilerplate Modal</Title>
+            <Text>Count: {count}</Text>
 
-            <div>
-              <button onClick={() => setCount((prev) => ++prev)}>Increment</button>
-              <button onClick={() => setCount((prev) => --prev)}>Decrement</button>
-              <button onClick={() => handleHideModal()}>Hide modal</button>
-            </div>
-          </div>
+            <Flex direction="row" justify="space-evenly">
+              <Button onClick={() => setCount((prev) => ++prev)}>Increment</Button>
+              <Button onClick={() => setCount((prev) => --prev)}>Decrement</Button>
+            </Flex>
+          </Modal>
         </div>
       )}
     </>
