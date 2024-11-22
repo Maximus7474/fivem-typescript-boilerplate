4ed21a2f2e5310fa513626115c17d089b0706a90
Maximus7474 <the.maximus.7474@gmail.com>
2024-11-22 11:21:17 +0100

    rf(web): imports in main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { isEnvBrowser } from './utils/misc.ts';
@@ -7,12 +7,12 @@ import { isEnvBrowser } from './utils/misc.ts';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <MantineProvider defaultColorScheme='dark'>
       <App />
     </MantineProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
 
 if (isEnvBrowser()) {
